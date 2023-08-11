842db3e2942e3f006bed0f95434be55ee3183450
Jamie Lovelace <pomponian@gmail.com>
2023-08-11 11:28:18 +0000

    wooo

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers'
 
-function Guess({ value }) {
+function Guess({ value, answer }) {
+  const check = checkGuess(value, answer)
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
+        <span key={num} className={`cell ${value && check[num].status}`}>
           {value ? value[num] : undefined}
         </span>
       ))}
